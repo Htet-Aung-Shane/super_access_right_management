6b578739cd5760aa592aa563f1d93ba9fdd7d704
Htet Aung <mmc.htetaungshane@gmail.com>
2025-10-11 00:30:33 +0630

    Add Screenshot and Description

--- super_access_right_management/__manifest__.py
+++ super_access_right_management/__manifest__.py
@@ -7,9 +7,13 @@
     'application': True,
     'installable': True,
     "auto_install": False,
+    "version": "18.0.1.0.0",
+    "price": 120.00,
+    "currency": "USD",
     'depends': ['web', 'base'],
     "data": [
         'security/ir.model.access.csv',
+        'security/access_super_access_right_mgmt.xml',
         'views/access_right_mgmt.xml',
         'views/res_users.xml',
     ],
@@ -19,6 +23,16 @@
         ],
 
     },
+    'images': [
+        'static/description/banner.png',
+        'static/description/screenshot1.png',
+        'static/description/screenshot2.png',
+        'static/description/screenshot3.png',
+        'static/description/screenshot4.png',
+        'static/description/screenshot5.png',
+        'static/description/screenshot6.png',
+        'static/description/screenshot7.png',
+    ],
     'module_type': 'official',
     'post_init_hook': 'post_install_action_dup_hook'
 }
